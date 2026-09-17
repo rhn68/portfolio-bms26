c46ce6d57868f83e8fd7567a4be2c496822c2dd2
Rizqy <rizqyhanggi07@gmail.com>
2026-09-17 13:39:19 +0700

    Initial commit with HTML and CSS

---
+++ script.js
@@ -0,0 +1,18 @@
+function sayHello() {
+    alert("Saya masih pemula.");
+}
+
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+sections.forEach(section => {
+    section.classList.add("hidden");
+    observer.observe(section);
+});
